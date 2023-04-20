a87d9b963a0f5a40e0e3189ffb5fe53322b61285
Adebozz <ademolaadeigbe3@gmail.com>
2023-04-20 09:12:38 +0100

    third commit

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -2,8 +2,8 @@ import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import { abi, NFT_CONTRACT_ADDRESS } from "../constants";
-import styles from "../styles/Home.module.css";
+import { abi, NFT_CONTRACT_ADDRESS } from "../../constants";
+import styles from "../globe/Home.module.css";
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
@@ -158,7 +158,7 @@ export default function Home() {
       </button>
     );
   };
- 
+
   return (
     <div>
       <Head>
@@ -183,7 +183,9 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>Made with &#10084; by LW3Punks</footer>
+      <footer className={styles.footer}>
+        Made with &#10084; by Dev Adeboss
+      </footer>
     </div>
   );
 }
